763fc881a86c80e635fab6f0a140d21e2e13219f
Mubi9000 <shabbirmuhammad2000@gmail.com>
2023-09-02 18:32:20 -0400

    Fixed issue with not being able to get specififc product id data

--- Routes/mens.js
+++ Routes/mens.js
@@ -7,14 +7,17 @@ router.get('/tshirts', (req, res) => {
 })
 
 router.get('/tshirts/:id', (req, res) => {
-  const productId = req.params.id
-  const product = mensTShirts.find((product) => product.id === productId)
+  const productId = parseInt(req.params.id);
 
-  if(!product) {
-    return res.status(404).json({ message: 'Product not found'})
+  // Verify that a product with this ID exists in mensTShirts
+  const product = mensTShirts.find((product) => product.id === productId);
+
+  if (!product) {
+    return res.status(404).json({ message: 'Product not found' });
   }
 
-  res.json(product)
-})
+  res.json(product);
+});
+
 
 module.exports = router
