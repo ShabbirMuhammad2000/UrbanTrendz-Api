658d09a16f9451bce079e7a7219764d4efe8dce0
Mubi9000 <shabbirmuhammad2000@gmail.com>
2023-09-08 17:14:41 -0400

    Working on implmenting api setpoints for backend

--- Routes/mens.js
+++ Routes/mens.js
@@ -20,4 +20,59 @@ router.get('/tshirts/:id', (req, res) => {
 });
 
 
+
+router.get('/shorts', (req, res) => {
+  res.json(mensShorts)
+})
+
+router.get('/shorts/:id', (req, res) => {
+  const productId = parseInt(req.params.id);
+
+  // Verify that a product with this ID exists in mensShorts
+  const product = mensShorts.find((product) => product.id === productId);
+
+  if (!product) {
+    return res.status(404).json({ message: 'Product not found' });
+  }
+
+  res.json(product);
+});
+
+router.get('/jeans', (req, res) => {
+  res.json(mensJeans)
+})
+
+router.get('/jeans/:id', (req, res) => {
+  const productId = parseInt(req.params.id)
+
+  // Verify that a porduct with this ID exists in mensJeans
+  const product = mensJeans.find((product) => product.id === productId)
+
+  if(!product) {
+    return res.status(404).json({message: 'Product not found'})
+  }
+
+  res.json(product)
+})
+
+
+router.get('/sweatpants', (req, res) => {
+  res.json(mensSweatpants)
+})
+
+router.get('/sweatpants/:id', (req, res) => {
+  const productId = parseInt(req.params.id)
+
+  // Verify that a porduct with this ID exists in MensSweatpants
+  const product = mensSweatpants.find((product) => product.id === productId)
+
+  if(!product) {
+    return res.status(404).json({message: 'Product not found'})
+  }
+
+  res.json(product)
+})
+
+
+
 module.exports = router
